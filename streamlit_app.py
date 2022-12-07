b1c85a48902d3b3e7b64a6f3cff8ebe104890176
sophsac <100931406+sophsac@users.noreply.github.com>
2022-12-07 17:33:07 +0000

    Update streamlit_app.py
    created a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,6 +26,15 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page
 streamlit.dataframe(fruits_to_show)
 
+# create a repeatable code block / function
+def get_fruityvice_data(this_fruit_choice):
+  # import requests
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
+  # take the json version of the fruityvice_response and normalise it 
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return fruityvice_normalized
+  
 # new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 try:
@@ -34,11 +43,7 @@ try:
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information.")
   else: 
-    # import requests
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    # streamlit.text(fruityvice_response.json()) # just writes the data to the screen
-    # take the json version of the fruityvice_response and normalise it 
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    back_from_function = get_fruityvice_data(fruit_choice)
     # creates dataframe of normalised fruityvice_response
     streamlit.dataframe(fruityvice_normalized)
     
