8ca8bb7caf35a899feebf4ac5214efa74059bad9
sophsac <100931406+sophsac@users.noreply.github.com>
2022-12-07 17:43:56 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,7 +51,7 @@ except URLError as e:
   streamlit.error()
 
 # dont run anything past here while we troubleshoot
-streamlit.stop()
+#streamlit.stop()
 
 # tells py file to use the library (Requirements) that's been added to the project
 # import snowflake.connector
