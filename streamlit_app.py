e257a46e49d9bb485302be056d151b9e75fafe5e
sophsac <100931406+sophsac@users.noreply.github.com>
2022-12-07 17:48:50 +0000

    Update streamlit_app.py
    created another function and if statement
    connected the list of fruits to snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,10 +68,17 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-
-# adds text entry box 
-fruit_added = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding ', fruit_added)
+  
+# allow user to add fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('from streamlist')")
+    return "Thanks for adding" + new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  streamlit.text(back_from_function)
 
 # will not work correctly initially
-my_cur.execute("insert into fruit_load_list values ('from streamlist')")
+# my_cur.execute("insert into fruit_load_list values ('from streamlist')")
